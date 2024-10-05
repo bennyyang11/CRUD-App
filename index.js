f017dfb9d775beab3ca2c2624af901c7212d77c7
bennyyang11 <bennyyang11@gmail.com>
2024-10-05 14:43:57 -0600

    Finished CRUD project

--- index.js
+++ index.js
@@ -1,52 +1,35 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose');
-const Product = require('./models/product.model.js');
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.route.js");
+const app = express();
 
+// middleware
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 
-app.listen(3000, () => {
-        console.log('Serve is running on port 3000');
-});
+// routes
+app.use("/api/products", productRoute);
 
-app.get('/',(req, res) => {
-    res.send('Hello from Node API ')
-});
 
-app.get('/api/products',async (req, res) => {
-    try {
-        const products = await Product.find({});
-        res.status(200).json(products);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-});
 
-app.get('/api/product/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
-})
 
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product)
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
+app.get("/", (req, res) => {
+  res.send("Hello from Node API Server Updated");
 });
 
 
-mongoose.connect("mongodb+srv://benjaminyangcareer:ruibiao725@backenddb.v4mvq.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
-.then(() => {
+mongoose
+  .connect(
+    "mongodb+srv://haris2iftikhar:GClTzr15XhkjvN6k@backenddb.nrurtot.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
     console.log("Connected to database!");
-})
-.catch(() => {
-    console.log("Connection Failed!");
-});
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
